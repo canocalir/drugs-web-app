51bfc0b903eeeb3bebe07e67584bc9ae19d39159
Can Berk Ocalir <can.ocalir@gmail.com>
2022-10-14 23:04:02 +0300

    fixed copyright

--- src/components/Footer/components/Copyright/Copyright.jsx
+++ src/components/Footer/components/Copyright/Copyright.jsx
@@ -2,7 +2,7 @@ import style from './Copyright.module.scss'
 
 const Copyright = () => {
   return (
-    <p className={style.copyright}>Designed By <a href="#">CodeTricks</a> © 2022</p>
+    <p className={style.copyright}>Designed By <a href="https://medium.com/code-tricks">CodeTricks</a> © 2022</p>
   )
 }
 
